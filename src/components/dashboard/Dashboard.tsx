02a0273cadbe878a8e13e4b5916a82e741d9121a
lightpc <afoageektech@gmail.com>
2024-11-20 10:44:42 +0100

    refactoring for small screens responsiveness

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -1,39 +1,41 @@
-import "./dashboard.module.css";
+import styles from "./dashboard.module.css";
 import Image from "next/image"
 
-export const Dashboard = () => {
+const Dashboard = () => {
   return (
-    <div className="dashboard">
-      <div className="texts">
-        <div className="logo">
-          <Image src="/logo.png" alt="logo" width={400} height={400} />
-          <h1>Voltz AI</h1>
+    <div className={styles.dashboard}>
+      <div className={styles.texts}>
+        <div className={styles.logo}>
+          <Image className={styles.image} src="/logo.png" alt="logo" width={400} height={400} />
+          <h1 className={styles.title}>Voltz AI</h1>
         </div>
-        <div className="options">
-          <div className="option">
-            <Image src="/chat.png" alt="chat image" width={400} height={400} />
+        <div className={styles.options}>
+          <div className={styles.option}>
+            <Image className={styles.image} src="/chat.png" alt="chat image" width={400} height={400} />
             <span>Create a New Chat</span>
           </div>
-          <div className="option">
-            <Image src="/image.png" alt="chat image" width={400} height={400} />
+          <div className={styles.option}>
+            <Image className={styles.image} src="/image.png" alt="chat image" width={400} height={400} />
             <span>Analyse Image</span>
           </div>
-          <div className="option">
-            <Image src="/code.png" alt="chat image" width={400} height={400} />
+          <div className={styles.option}>
+            <Image className={styles.image} src="/code.png" alt="chat image" width={400} height={400} />
             <span>Help me with my code</span>
           </div>
         </div>
       </div>
-      <div className="formContainer">
-        <form>
-          <input type="text" placeholder="Ask me anything..."/>
-          <button type="submit">
-            <Image src="/arrow.png" alt="arrow image" width={400} height={400}/>
+      <div className={styles.formContainer}>
+        <form className={styles.form}>
+          <input className={styles.input} type="text" placeholder="Ask me anything..."/>
+          <button className={styles.button} type="submit">
+            <Image className={styles.image} src="/arrow.png" alt="arrow image" width={400} height={400}/>
           </button>
         </form>
       </div>
     </div>
   );
 };
+
+export default Dashboard
 
 
